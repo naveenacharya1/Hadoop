92242e7ab13265d1d913a1364e59de8134d1a1d3
Naveen <naveen.c.acharya@gmail.com>
2016-09-05 14:20:27 +0800

    map reduce

---
+++ HadoopMapReduce/src/main/java/com/navi/hdp/mapreduce/WordCount.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.navi.hdp.mapreduce;
+
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCount {
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		
+		conf.set("fs.hdfs.impl",org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+	    conf.set("fs.file.impl",org.apache.hadoop.fs.LocalFileSystem.class.getName());
+		Path inputPath = new Path("hdfs://127.0.0.1:9000/in/wordcount.txt");
+		Path outputPath = new Path("hdfs://127.0.0.1:9000/out/result");
+
+		@SuppressWarnings("deprecation")
+		Job job = new Job();
+		job.setJarByClass(WordCount.class);
+		job.setJobName("WordCounterJob");
+
+		FileInputFormat.addInputPath(job, inputPath);
+		FileOutputFormat.setOutputPath(job, outputPath);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+
+		FileSystem hdfs = FileSystem.get(URI.create("hdfs://127.0.0.1:9000"), conf);
+		if (hdfs.exists(outputPath))
+			hdfs.delete(outputPath, true);
+
+		int returnValue = job.waitForCompletion(true) ? 0 : 1;
+		System.out.println("job.isSuccessful " + job.isSuccessful());
+		System.exit(returnValue);
+	}
+
+}
---
+++ HadoopWordCount/src/main/java/com/nav/hadoop/exmaple/wordcount/TestMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.nav.hadoop.exmaple.wordcount;
+
+/**
+ * @author Naveen
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test");
+	}
+
+}
