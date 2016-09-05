856da80a748866e39570ce2a2695be46489c3300
Naveen <naveen.c.acharya@gmail.com>
2016-09-05 14:29:05 +0800

    Word Count

---
+++ hadoop-map-reduce/WorkCount/src/main/java/com/navi/hadoop/mapreduce/exmaples/App.java
@@ -0,0 +1,13 @@
+package com.navi.hadoop.mapreduce.exmaples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
