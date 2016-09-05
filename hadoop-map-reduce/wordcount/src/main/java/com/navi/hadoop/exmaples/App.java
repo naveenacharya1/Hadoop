b97cd8a53ac178e98b8f87c8ec55ee0e419107e1
Naveen <naveen.c.acharya@gmail.com>
2016-09-05 14:35:23 +0800

    wc exmaples

---
+++ hadoop-map-reduce/wordcount/src/main/java/com/navi/hadoop/exmaples/App.java
@@ -0,0 +1,13 @@
+package com.navi.hadoop.exmaples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
