837bc2ae50f899e4623367138e0ec8480ace10f4
Naveen <naveen.c.acharya@gmail.com>
2016-09-05 14:40:59 +0800

    Modified

--- hadoop-map-reduce/wordcount/src/main/java/com/navi/hadoop/exmaples/WordCount.java
+++ hadoop-map-reduce/wordcount/src/main/java/com/navi/hadoop/exmaples/WordCount.java
@@ -19,9 +19,7 @@ public class WordCount {
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		
-		conf.set("fs.hdfs.impl",org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-	    conf.set("fs.file.impl",org.apache.hadoop.fs.LocalFileSystem.class.getName());
+
 		Path inputPath = new Path("hdfs://127.0.0.1:9000/in/wordcount.txt");
 		Path outputPath = new Path("hdfs://127.0.0.1:9000/out/result");
 
