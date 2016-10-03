e07829c1838a36c6c117af64dba8f9c124bf6d96
Naveen <naveen.c.acharya@gmail.com>
2016-10-03 21:22:12 +0800

    Map reduce Join

---
+++ hadoop-map-reduce/MapSideJoinMR/src/main/java/org/MapSideJoinMR/App.java
@@ -0,0 +1,13 @@
+package org.MapSideJoinMR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
